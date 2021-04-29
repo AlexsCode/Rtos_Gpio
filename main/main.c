f68e4f2812055b9af0ead74717a600868ed51c7e
AlexsCode <alex.tuddenham.at@googlemail.com>
2021-04-29 16:30:28 +0100

    Added Timer_intr Example

--- main/main.c
+++ main/main.c
@@ -12,255 +12,167 @@
 #include "freertos/task.h"
 #include "freertos/queue.h"
 #include "driver/gpio.h"
+#include "driver/timer.h"
 
-#define HIGH 1
-#define LOW 0
-#define INPUT 0
-#define OUTPUT 1
-#define DISABLE 0
-#define ENABLE 1
+#define TIMER_DIVIDER 16
+#define TIMER_SCALE (TIMER_BASE_CLK/TIMER_DIVIDER)
 
 
-
-//Queue Handler/event queue;
-static xQueueHandle gpio_event_queue = NULL; // setting up a Queue.
-
-static xQueueHandle new_event_queue = NULL; // setting up a  new Queue
-
-static xQueueHandle ten_values_queue = NULL;
-
-static TaskHandle_t gpio_handler1 = NULL;
-
-
-//aka Queue_handle_t
-
-//task B
-
-//ISR Handler A
+typedef struct 
+{   
+    int timer_group;
+    int timer_idx;
+    int alarm_interval;
+    bool auto_reload;
+}example_timer_info_t;
 
 /**
- * @brief 
- *     static to make it always be there.
-    void because it has no expected return time.
-    gpio_isr_handler because its and interupt service routine handler.
-    void * argument because its passed via a pointer arg.
+ * @brief Nested Structure for timer Events.
  * 
- * @param argument this value is the gpio number in this context
  */
-static void IRAM_ATTR gpio_isr_handler_A(void * argument)
-{ 
-
-    uint32_t gpio_number = (uint32_t) argument; 
-    //the arg is the pin the gpio interrupt occurs on.
-    //This is being cast to 32 bit and saved as gpio number.
-    xQueueSendFromISR(gpio_event_queue, &gpio_number,NULL);
-    //replace null if you want to trigger another task from this.
-    //sends to back of queue and is safe to use withing a ISR.
-    //queued by copy not reference so only good for small items.
-    //better to only store a pointer to items being queued.
-}
-
-//ISR Handler B
-static void IRAM_ATTR gpio_isr_handler_B(void * argument)
-{ 
-
-    uint32_t gpio_number = (uint32_t) argument; 
-    //the arg is the pin the gpio interrupt occurs on.
-    //This is being cast to 32 bit and saved as gpio number.
-    xQueueSendFromISR(new_event_queue, &gpio_number,NULL);
-    //replace null if you want to trigger another task from this.
-    //sends to back of queue and is safe to use withing a ISR.
-    //queued by copy not reference so only good for small items.
-    //better to only store a pointer to items being queued.
-}
+typedef struct 
+{
+    example_timer_info_t info;
+    uint64_t timer_counter_value;
+}example_timer_event_t;
 
 
-//task A
+static xQueueHandle s_timer_queue;
+
 /**
- * @brief task just waits and loops, looking for items on the event queue, every 50 ticks.
- * Once recieved it then saves it to a buffer.
- * If its recieved then prints the message saying which pin created interrupt and what level.
+ * @brief Timer Counter - Print Look
  * 
- * @param argument pointer to task
+ * @param counter_value 
  */
-static void gpio_task_1(void * argument)
+static void inline print_timer_counter(uint64_t counter_value)
 {
-    uint32_t io_number_buffer; //creates a buffer for events?
-
-    for(;;){
-        if(xQueueReceive(gpio_event_queue, &io_number_buffer,portMAX_DELAY))
-        {
-            printf("GPIO %d interrupt, value: %d \n",io_number_buffer,gpio_get_level(io_number_buffer));
-            //prints the pin number and then pulls second value from buffer which is the value of pin.
-
-        }
-    }
+    printf("Counter: 0x%08x%08x\r\n",(uint32_t)  (counter_value>>32),
+    (uint32_t)(counter_value));
+    printf("Time : %.8f s\r\n",(double)counter_value / TIMER_SCALE);
 }
 
-static void gpio_task_2(void * argument)
+//Timer Group CallBack
+
+static bool IRAM_ATTR timer_group_isr_callback(void *argument)
 {
-    uint32_t io_number_buffer; //creates a buffer for events?
+    BaseType_t high_task_awoken = (BaseType_t) false;
+    //Setting Base Type as Zero;
+    example_timer_info_t *info = (example_timer_info_t *) argument;
+    //Creating a pointer - Cast the argument to be example_timer_t.
 
-    for(;;){
-        if(xQueueReceive(new_event_queue, &io_number_buffer,portMAX_DELAY))
-        {
-            printf("GPIO %d interrupt, value: %d \n",io_number_buffer,gpio_get_level(io_number_buffer));
-            //prints the pin number and then pulls second value from buffer which is the value of pin.
+    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(
+        (info->timer_group),(info->timer_idx));
+    //Pulling via the passed arg assuming its structure of example_timer_info.
 
-        }
-    }
-}
-
-static void gpio_task_3(void * argument)
-{
-    int counter=0;
-    for (;;)
+    example_timer_event_t sEventTime_obj=
     {
-        counter = counter +1;
-        if(ten_values_queue !=0){
+        .info.timer_group   = info->timer_group,
+        .info.timer_idx     = info->timer_idx,
+        .info.alarm_interval= info->alarm_interval,
+        .info.auto_reload   = info->auto_reload,
+        .timer_counter_value= timer_counter_value
 
-            xQueueSend(ten_values_queue,(void *) &counter,portMAX_DELAY);
-        }
+    };
+    //Create and Allocate Structure
 
-
-    }
-}
-
-static void gpio_task_4(void * argument)
-{
-    uint32_t buffer_FIFO=0;
-    for(;;)
+    if(!(info->auto_reload))
     {
-        if(ten_values_queue !=0)
-        {
-            if(xQueuePeek(ten_values_queue,(void *)buffer_FIFO ,(TickType_t)10))
-            {
-                printf("%d\n",buffer_FIFO);
-                vTaskDelay(500);
-            }
-        }
+        timer_counter_value += (info->alarm_interval*TIMER_SCALE);
+        timer_group_set_alarm_value_in_isr(
+                                            info->timer_group,
+                                            info->timer_idx,
+                                            timer_counter_value);
     }
 
+    xQueueSendFromISR(s_timer_queue,&sEventTime_obj,&high_task_awoken);
+    //Sends Event Data back via queue
+
+    return high_task_awoken == pdTRUE;
+    //Decides whether to yield at end to a higher Interupt
+
 }
+//Create Timers || Timer Group.
 
 
+static void timer_group_timer_init(int16_t group, int16_t timer,
+                    bool auto_reload_param , int16_t timer_interval_sec)
+{
+    //Setting up a default config via timer_types.h
+    timer_config_t config = 
+    {
+        .divider = TIMER_DIVIDER,
+        .alarm_en = TIMER_ALARM_EN,
+        .counter_en = TIMER_PAUSE,
+        .auto_reload = auto_reload_param,
+        .counter_dir = TIMER_COUNT_UP ,
+    };
+
+    timer_init(group,timer,&config);
+    // creates a timer with group assigned , timer passed and default config.
+
+    timer_set_counter_value(group , timer, 0);
+    //sets value at the timer being passed in.
+    //this will be the value it initialises from.
+
+
+    timer_set_alarm_value(group,timer,(timer_interval_sec)*TIMER_SCALE);
+    //Sets the time when the interupt Alarm hits
+    timer_enable_intr(group,timer);
+    //Turns the interupt on... This is all ready to go.
+
+
+    example_timer_info_t *timer_info = calloc(1,sizeof(example_timer_info_t));
+
+    timer_info->timer_group = group;
+    timer_info->timer_idx   = timer;
+    timer_info->auto_reload = auto_reload_param;
+    timer_info->alarm_interval = timer_interval_sec;
+    timer_isr_callback_add(group,timer,timer_group_isr_callback,timer_info,0);
+    //Adds the timers to the callback.
+    
+    timer_start(group,timer);
+    
+}
 
 
 
 void app_main(void)
 {
-    //configuration of registers.
-    
-    //esp port can do registers via their own structures.
-    //to configure the peripherals we use a gpio_config_t structure.
+    s_timer_queue = xQueueCreate(10,sizeof(example_timer_event_t));
 
-    gpio_config_t gpio_configuration_object;
-    //creating config object
+    timer_group_timer_init(TIMER_GROUP_0,TIMER_0,pdTRUE,3);
+    timer_group_timer_init(TIMER_GROUP_1,TIMER_0,pdFALSE,5);
 
-    //********************
-    //Configuring 
 
-    gpio_configuration_object.intr_type=DISABLE;
-    //setting whether interupt type or not.
+    for(;;)
+    {
+        example_timer_event_t sEventTime_obj;
+        xQueueReceive(s_timer_queue,&sEventTime_obj,portMAX_DELAY);
 
-    //gpio_configuration_object.mode = OUTPUT;
-    gpio_configuration_object.mode = GPIO_MODE_OUTPUT;
-    //sets the perpheral register to output.
-    
-    gpio_configuration_object.pin_bit_mask = ((1ULL<<18)|(1ULL<<19));
-    //bit mask is LSL with the number of pin. can be either 18 or 19.
-    //effectively setting bits 18 and 19 to true.
+        if(sEventTime_obj.info.auto_reload)
+        {
+            printf("Timer Group - Auto Reload ON\n");
+        }
+        else
+        {
+            printf("Timer Group - Auto Reload OFF\n");
+        }
 
-    gpio_configuration_object.pull_down_en = DISABLE;
-    //disables pull down
+        printf("Group [%d],timer[%d],alarm\n",sEventTime_obj.info.timer_group,
+                                              sEventTime_obj.info.timer_idx);
+        
+        printf("---------EVENT_TIME-------");
+        print_timer_counter(sEventTime_obj.timer_counter_value);
 
-    gpio_configuration_object.pull_up_en = DISABLE;
-    //disables pull up mode.
+        printf("---------TASK_TIME-------");
+        uint64_t task_counter_value;
+        timer_get_counter_value(sEventTime_obj.info.timer_group,
+                                sEventTime_obj.info.timer_idx,
+                                &task_counter_value);
+        //puts the task counter value into this func.
 
-    gpio_config(&gpio_configuration_object);
-    //saves structure as a peripherals setting. (Register setup)
-
-    //*************************
-    //configuring inputs
-
-    gpio_configuration_object.intr_type =1;
-    //setting as interupt type.
-
-    gpio_configuration_object.pin_bit_mask = ((1ULL<<5)|(1ULL<<4));
-    //bit mask for pins 4 and 5.
-
-    //gpio_configuration_object.mode = INPUT;
-    gpio_configuration_object.mode = GPIO_MODE_DEF_INPUT;
-    //setting to input bits.
-
-    gpio_configuration_object.pull_up_en= HIGH;
-    //setting pull up mode on.
-    gpio_config(&gpio_configuration_object);
-
-    //******************
-
-    gpio_set_intr_type(4,GPIO_INTR_ANYEDGE);
-
-    //GPIO_INTR_ANYEDGE = enum of 3, setting rise/
-    //GPIO POS EDGE = 1
-    //GPIO NEG EDGE = 2
-    //GPIO Disable = 0
-    //GPIO INTR HIGH_ Level = 5
-
-    //static uint8_t ucParameterToPass;//added empty parameter
-
-    //create a freeRtos queue
-    gpio_event_queue = xQueueCreate(10,sizeof(uint32_t));
-    //creates queue of length 10 and max size 32bit
-
-     //create a freeRtos queue
-    new_event_queue = xQueueCreate(10,sizeof(uint32_t));
-    //creates queue of length 10 and max size 32bit
-
-    ten_values_queue = xQueueCreate(10,sizeof(uint32_t));
-
-    //gpio task.
-    xTaskCreate(gpio_task_1,"gpio_task_1",2048,NULL,10,NULL);
-    //not got a task handler.
-
-    //gpio task.
-    xTaskCreate(gpio_task_2,"gpio_task_2",2048,NULL,9,&gpio_handler1);
-    //not got a task handler.
-
-    xTaskCreate(gpio_task_3,"gpio_task_3",2048,NULL,2,NULL);
-    xTaskCreate(gpio_task_4,"gpio_task_4",2048,NULL,4,NULL);
-
-    //gpio isr.
-    /**
-     * @brief creates ISR looking for interupt allocation flag.
-     * setting flag to 0.
-     * 
-     */
-    gpio_install_isr_service(0);
-
-    /**
-     * @brief creates a link for the interupt handler.
-     * If someting on pin 4, 
-     * ISR handler called with parameter typecast void * pin 4
-     * 
-     */
-    gpio_isr_handler_add(4,gpio_isr_handler_A,(void*)4);
-
-     /**
-     * @brief creates a link for the interupt handler.
-     * If someting on pin 4, 
-     * ISR handler called with parameter typecast void * pin 4
-     * 
-     */
-    gpio_isr_handler_add(5,gpio_isr_handler_B,(void*)5);
-
-    int cnt = 0;
-    while(1) {
-        printf("cnt: %d\n", cnt++);
-        vTaskDelay(1000 / portTICK_RATE_MS);
-        gpio_set_level(18, cnt % 2);
-        gpio_set_level(19, cnt % 2);
-
+        print_timer_counter(task_counter_value);
+  
     }
- 
+
 }
